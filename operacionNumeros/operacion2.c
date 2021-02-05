10701ee2b09645b127148df5fd67edfba8f4456d
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-04 21:48:15 -0600

    ejemplo operacion2

---
+++ operacionNumeros/operacion2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+    int num1, num2, num3, num4, suma, producto;
+
+    printf("Digite num1: ");
+    scanf("%i",&num1);
+    printf("Digite num1: ");
+    scanf("%i",&num2);
+    printf("Digite num1: ");
+    scanf("%i",&num3);
+    printf("Digite num1: ");
+    scanf("%i",&num4);
+
+    suma = num1 + num2;
+    producto = num3 * num4;
+
+    printf("Suma: %i",suma);
+    printf("\nProducto: %i",producto);
+
+    printf("\n");
+
+    return 0;
+}
