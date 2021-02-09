7d090227741283ed7aae1014dac73d77301e1e44
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-08 23:41:19 -0600

    ejercicio datos de una persona

---
+++ Curso_C++/LecturaDatos/datos2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int edad;
+    char sexo;
+    float altura;
+
+    cout<<"Digite su edad: ";
+    cin>>edad;
+    cout<<"Digite su sexo F/M: ";
+    cin>>sexo;
+    cout<<"Digite su altura: ";
+    cin>>altura;
+    cout<<"\n\n\n";
+    cout<<"Su edad es: "<<edad<<endl;
+    cout<<"Su sexo es: "<<sexo<<endl;
+    cout<<"Su altura es: "<<altura<<endl<<endl;
+    
+    return 0;
+}
