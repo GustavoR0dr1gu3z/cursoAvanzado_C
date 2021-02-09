531cf9dc17156a9b70257145471cce30b0a3e239
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-08 22:28:22 -0600

    ejercicio suma en c++

---
+++ Programacion_Paralela/C++/suma.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+//argc = Rencuentro de argumentos
+//argv = vector de argumentos
+int main(int argc, char *argv[]){
+    int suma = 0;
+    int a,b,c;
+
+    a = atoi(argv[1]);
+    b = atoi(argv[2]);
+    c = atoi(argv[3]);
+    suma = a+b+c;
+
+    cout<<"La suma de los: " <<argc-1<<" sumandos es: "<<endl;
+    cout<<suma<<endl;    
+    
+    return 0;
+}
