0f6a960f79ab60a34ae6d14def9bc80ec6c3a949
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-09 23:09:22 -0600

    ejercicio 4 expresiones matematicas

---
+++ Curso_C++/Expresiones/expresion4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float a,b,c,d, res=0;
+
+    cout<<"Digite el valor de a: "; cin>>a;
+    cout<<"Digite el valor de b: "; cin>>b;
+    cout<<"Digite el valor de c: "; cin>>c;
+    cout<<"Digite el valor de d: "; cin>>d;
+
+
+    res = ( a+((b)/(c-d))  );
+
+    cout<<"\nEl resultado es: "<<res<<endl<<endl;
+
+    return 0;
+}
+
