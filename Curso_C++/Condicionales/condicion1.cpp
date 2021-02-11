8f249c71d59d47d72fe0d1e2c5201e25c38b24e2
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-10 23:56:16 -0600

    ejercicio condicionales

---
+++ Curso_C++/Condicionales/condicion1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int digito = 5, num = 0;
+
+    cout<<"Digite un numero para comparar: "; cin>>num;
+
+    if(num == digito){
+        cout<<"\nEl numero coincide"<<endl;
+    }else{
+        cout<<"\nEl numero NO coincide"<<endl;
+    }
+
+
+    return 0;
+}
