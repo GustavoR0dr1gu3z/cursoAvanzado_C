02bd16b3437341a9b1a9521f9b0aeeac7a63d6b9
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-16 23:43:49 -0600

    creando los 4 hilos

--- Programacion_Paralela/C++/Crear_4hilos.cpp
+++ Programacion_Paralela/C++/Crear_4hilos.cpp
@@ -2,6 +2,8 @@
 #include <cstdio>
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 
 
 using namespace std;
@@ -9,6 +11,7 @@ using namespace std;
 //Estructura de h1, leer archivo
 struct read_file{
     int countR;
+    string fraseR;
 };
 
 //Estructura de h2, escribir archivo
@@ -38,7 +41,7 @@ struct read_file* r = (struct read_file*) parametros;
     int i;    
     for(i=0; i<r->countR;i++)
         {
-            cout<<"ARCHIVO LEIDO";
+            cout<<r->fraseR;
         }  
     return NULL;
 
@@ -52,38 +55,80 @@ struct write_file* w = (struct write_file*) parametros;
     int i;    
     for(i=0; i<w->countW;i++)
         {
-            cout<<"ARCHIVO ESCRITO";
+            cout<<w->fraseW;
         }  
     return NULL;
 
 }
 // Metodo imprime el contenido en terminal
-void* imprime_Terminal(void* parametros){
+void* imprime_terminal(void* parametros){
 
 struct print_phrase* p = (struct print_phrase*) parametros;
     
     int i;    
     for(i=0; i<p->countP;i++)
         {
-            cout<<"Frase Mostrada";
+            cout<<p->fraseP;
         }  
     return NULL;
 
 }
 // Metodo para leer archivo
-void* numerosAleatorios(void* parametros){
+void* numeros_aleatorios(void* parametros){
 
 struct random_number* n = (struct random_number*) parametros;
     
     int i;    
     for(i=0; i<n->countN;i++)
         {
-            cout<<"Frase leída";
+            cout<<n->numerosN;
         }  
     return NULL;
 
 }
 
+int main(){
+
+    int numero, c;
+    srand(time(NULL));
+
+    numero = 1+rand()%(11-1);
+
+    pthread_t t1;
+    pthread_t t2;    
+    pthread_t t3;    
+    pthread_t t4;    
+
+    struct read_file t1_args;
+    struct write_file t2_args;
+    struct print_phrase t3_args;
+    struct random_number t4_args;
+
+
+    t1_args.countR = 10000;
+    pthread_create(&t1,NULL,&leer_archivo,&t1_args);
+
+    t2_args.fraseW = 'Escribiendoooo ';
+    t2_args.countW = 10000;
+    pthread_create(&t2,NULL,&escribir_archivo,&t2_args);
+
+    t3_args.fraseP = 'Hola Hilos ';
+    t3_args.countP = 10000;
+    pthread_create(&t3,NULL,&imprime_terminal,&t3_args);
+
+    t4_args.numerosN = numero;
+    t4_args.countN = 10000;
+    pthread_create(&t4,NULL,&numeros_aleatorios,&t4_args);
+
+
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    pthread_join(t3,NULL);
+    pthread_join(t4,NULL);
+
+
+    return 0;
+}
 
 
 
