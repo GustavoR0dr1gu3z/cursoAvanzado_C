963b16d47246b90cb1e6d0f60fe74090097c63b8
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-17 22:56:52 -0600

    arreglando gauss

--- Programacion_Paralela/C++/Gauss/gaus.cpp
+++ Programacion_Paralela/C++/Gauss/gaus.cpp
@@ -4,8 +4,17 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#include "gauss_fun212.h"
-
+//#include "gauss_fun212.h"
+float** crea_mat(int, int);
+float* crea_vect(int);
+int muestra_mat(float**, int, int);
+int muestra_vect(float*, int);
+void* mult_hilo(void*);
+float** lee_mat(char*, int, int);
+float* lee_vect(char*, int);
+int ini_vect(float*, int);
+float* eval_ret(float**, int, int);
+int guarda_mat(float**, int, int, char*);
 
 using namespace std;
 
