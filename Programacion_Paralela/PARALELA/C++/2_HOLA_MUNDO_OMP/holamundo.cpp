15e0dc0f0d88c538b40d50ef14651ff510d8b6f3
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-16 12:18:53 -0500

    hola mundo omp

--- Programacion_Paralela/PARALELA/C++/2_HOLA_MUNDO_OMP/holamundo.cpp
+++ Programacion_Paralela/PARALELA/C++/2_HOLA_MUNDO_OMP/holamundo.cpp
@@ -1,9 +1,15 @@
+// EJERCICIO 2
+
+// Se compila con: g++ -o holamundoCompilado holamundo.cpp -fopenmp
+// Se ejecuta con: ./holamundoCompilado
+
 #include <iostream>
 #include <omp.h>
 
 using namespace std;
 int main(){
-
+// Si quiero mas 
+    omp_set_num_threads(10);
     #pragma omp parallel
     {
         cout<<"Hola Mundo En Paralelo"<<endl;
