6d61827f1e8aacda099a91d6be0fade4c4999d83
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-18 20:39:24 -0500

    importando librerias

--- Programacion_Paralela/PARALELA/C++/4_VectorXVector/vv.cpp
+++ Programacion_Paralela/PARALELA/C++/4_VectorXVector/vv.cpp
@@ -1,1 +1,14 @@
-// 
+// EJERCICIO 4
+
+// Se compila con:
+
+// Se ejecuta con:
+
+// El resultado es un escalar
+
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
+#include <omp.h>
+#include <math.h>
