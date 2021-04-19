2ec09cd32765c57c5b2b7f54c0a6e0a621763b68
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-18 19:43:14 -0500

    paralela-for

---
+++ Programacion_Paralela/PARALELA/C++/3_PRODUCTO_OMP/producto.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+int main(){
+
+    int i;
+    //Private significa que cada hilo tendrá un cierto 
+    //numero de indices que ejecutar dentro del for
+    #pragma omp parallel for private(i){
+
+        for (i =0; i<20; i++){
+            cout<<"Hola mundo con omp: "<<i<<endl;
+        }
+
+    }
+
+
+}
