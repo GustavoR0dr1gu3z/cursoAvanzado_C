79d09d53450695ea7efd905e402cdd1fe0ca5f47
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-20 13:23:53 -0500

    hola con hilos-paralela

--- Programacion_Paralela/PARALELA/C++/6_DEFINICION_C_HILOS/hola.cpp
+++ Programacion_Paralela/PARALELA/C++/6_DEFINICION_C_HILOS/hola.cpp
@@ -11,19 +11,14 @@
 using namespace std;
 int main(){
 
-// Definit un entero k para la suma
-    int k = 0;
-
 // Si quiero mas impresiones en pantalla
     omp_set_num_threads(6);
 
     // Definimos una región paralela
     #pragma omp parallel 
     {
-        cout<<k<<endl; // Imprimir k
-        k += 1;
         cout<<"Hola Mundo En Paralelo"<<endl;
-        cout<<k<<endl; // Imprimir k
+
     }
     return 0;
 }
