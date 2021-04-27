3b0ccf1e662e63bbcb074585af1ca04fcbba52f2
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-26 21:32:08 -0500

    VxV usando Section

--- Programacion_Paralela/PARALELA/C++/8_SECTION_VxV/svv.cpp
+++ Programacion_Paralela/PARALELA/C++/8_SECTION_VxV/svv.cpp
@@ -12,6 +12,17 @@ float *V, *W, Prod = 0.0;
 int main(int argc, char *argv[]){
 
     int n, j;
+    char *nomA1;
+    char *nomA2;
+
+    n = atoi(agrv[1]);
+
+    nomA1 = argv[2];
+    nomA2 = argv[3];
+
+    V = crea_vec(n);
+    W = crea_vec(n);
+
     
 
 }
