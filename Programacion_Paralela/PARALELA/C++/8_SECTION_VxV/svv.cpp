5c1f498205b90d621e243b127b53f6d440f5fa84
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-26 21:29:45 -0500

    VxV usando Section

---
+++ Programacion_Paralela/PARALELA/C++/8_SECTION_VxV/svv.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <omp.h>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "fun_omp.h"
+
+using namespace std;
+
+float *V, *W, Prod = 0.0;
+
+int main(int argc, char *argv[]){
+
+    int n, j;
+    
+
+}
