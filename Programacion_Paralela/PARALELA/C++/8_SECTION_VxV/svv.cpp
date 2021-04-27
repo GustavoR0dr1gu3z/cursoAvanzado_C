15409b6694e8b30aa7faa4aa33d48bdb78e992b1
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-26 22:27:58 -0500

    archivo Make

--- Programacion_Paralela/PARALELA/C++/8_SECTION_VxV/svv.cpp
+++ Programacion_Paralela/PARALELA/C++/8_SECTION_VxV/svv.cpp
@@ -13,33 +13,34 @@ float *V, *W, Prod = 0.0;
 
 int main(int argc, char *argv[]){
 
-    int n, j;
+    int n, i;
     char *nomA1;
     char *nomA2;
 
-    n = atoi(agrv[1]);
+    n = atoi(argv[1]);
 
     nomA1 = argv[2];
     nomA2 = argv[3];
 
-    V = crea_vec(n);
-    W = crea_vec(n);
+
 
 // SECTIONS: LEER Y MOSTRAR VECTOR
-    #pragma omp parallel private(tid)
-        #pragma omp sections
+    //#pragma omp parallel private(tid)
+    #pragma omp parallel sections
+    {
+        #pragma omp section
         {
-            #pragma omp section
-            {
-                V = lee_vec(nomA1,n);
-                muestra_vec(V,n);
-            }
-            #pragma omp section 
-            {
-                W = lee_vec(nomA2,n);
-                muestra_vec(W,n);
-            }
+            V = crea_vec(n);
+            V = lee_vec(nomA1,n);
+            muestra_vec(V,n);
         }
+        #pragma omp section 
+        {
+            W = crea_vec(n);
+            W = lee_vec(nomA2,n);
+            muestra_vec(W,n);
+        }
+    }
 // OMP: PRODUCTO
     #pragma omp for reduction(+:Prod)
         for (i=0; i<n; i++){
