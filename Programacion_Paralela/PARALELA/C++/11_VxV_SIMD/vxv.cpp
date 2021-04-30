5fcf72b3f40f66ac169593a94aa131f97d98c989
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-30 13:17:31 -0500

    VxV SIMD

--- Programacion_Paralela/PARALELA/C++/11_VxV_SIMD/vxv.cpp
+++ Programacion_Paralela/PARALELA/C++/11_VxV_SIMD/vxv.cpp
@@ -7,4 +7,19 @@
 
 using namespace std;
 
+int vxv (float *a, float*b, int n){
+    int i;
+    #pragma omp simd reduction(+:c)
+        for(i=0; i<n; i++){
+            c = b[i]*a[i];
+        }
+return c;
+}
+
+int main(int argc, char *argv[]){
 float *a, *b, *c;
+
+
+cout<<"Resultado: "<<c<<endl;
+return 0;
+}
