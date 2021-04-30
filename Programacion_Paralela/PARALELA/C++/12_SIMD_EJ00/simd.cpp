fe52b40f98d00bf8a24b5e8680136c1f91fc3de8
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-30 12:37:45 -0500

    simd

--- Programacion_Paralela/PARALELA/C++/12_SIMD_EJ00/simd.cpp
+++ Programacion_Paralela/PARALELA/C++/12_SIMD_EJ00/simd.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <fstream>
 #include "fun_omp.h"
 
 void simd_loop(float *a, float *b, float *c, int n){
@@ -43,3 +44,36 @@ muestra_vec(c,n);
 return 0;
 }
 
+float *lee_vec(char* nom_arch, int m){
+    int i;
+    float *VV;
+// SE CREA EL VECTOR CON UN TAMAÑO ASIGNADO
+    VV = crea_vec(m);
+
+    fstream fd2;
+// ABRE EL ARCHIVO Y SE GUARDA EM VV
+    fd2.open(nom_arch, ios::in);
+    while(!fd2.eof()){
+        int i,j;
+        for(i=0; i<m; i++){
+            fd2>>VV[i];
+        }
+    }
+    fd2.close();
+    cout<<"Vector Leído"<<endl;
+    return VV;
+}
+
+float* crea_vec(int m){
+    float *VV;
+    VV = new float[m];
+    return VV;
+}
+
+int muestra_vec(float* M ,int n){
+    int i;
+    for (i=0; i<n; i++){
+        cout<<M[i]<<endl;
+    }
+    return 0;
+}
