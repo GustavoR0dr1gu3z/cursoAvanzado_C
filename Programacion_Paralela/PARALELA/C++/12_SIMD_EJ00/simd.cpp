c9c727ea5c581a210cc4717d7c2b2178afc9663a
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-30 12:27:08 -0500

    simd

--- Programacion_Paralela/PARALELA/C++/12_SIMD_EJ00/simd.cpp
+++ Programacion_Paralela/PARALELA/C++/12_SIMD_EJ00/simd.cpp
@@ -4,5 +4,20 @@
 #include <omp.h>
 #include "fun_omp.h"
 
+void simd_loop(float *a, float *b, float *c, int n){
+    int i;
+    #pragma omp simd
+        for (i=0; i<n; i++){
+            a[i] = b[i]+c[i];
+        }
+}
 
+int main(){
+
+
+
+
+
+return 0;
+}
 
