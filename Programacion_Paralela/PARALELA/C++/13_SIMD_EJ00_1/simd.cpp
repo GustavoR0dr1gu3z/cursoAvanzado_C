fb8dddd5887958513396bbd7b3f259cd9b4d90b3
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-01 22:09:43 -0500

    13.- SIMD Ejemplo 01 Suma Parte2

--- Programacion_Paralela/PARALELA/C++/13_SIMD_EJ00_1/simd.cpp
+++ Programacion_Paralela/PARALELA/C++/13_SIMD_EJ00_1/simd.cpp
@@ -1,5 +1,6 @@
 // EJERCICIO 13
 
+// Se compila como g++ -o simdCompilado simd.cpp -fopenmp
 // Se ejecuta con: ./simdCompilado 10 uno.txt dos.txt
 // time taskset -c 0 ./simdCompilado 10 uno.txt dos.txt 
 
