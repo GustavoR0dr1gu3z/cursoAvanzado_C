510ffeea4a617b21fcd7a0930b15e61d72e41481
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-04 13:36:43 -0500

    Calcular PI

---
+++ Programacion_Paralela/PARALELA/C++/16_CALCULAR_PI/pi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <omp.h>
+#include "fun_omp.h"
+#include <stdlib.h>
+
+float pi (int count){
+    float dpi = 0.0,t;
+    #pragma omp parallel for simd private(t) reduction(+:dpi)
+        for (int i=0; i<count;i++){
+            t = (float)((i+0.5)/count);
+            dpi += 4.0/(1.0+t*t);
+            dpi /= count;
+            
+        }
+        return dpi;
+}
+
