129274439f0d66640fe5569ed88cd35aeaae3468
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-04 13:14:20 -0500

    SIMD

--- Programacion_Paralela/PARALELA/C++/14_SIMD_SAXPY/simd.cpp
+++ Programacion_Paralela/PARALELA/C++/14_SIMD_SAXPY/simd.cpp
@@ -6,3 +6,16 @@
 using namespace std;
 
 #pragma omp declare simd
+
+    void saxpy(float *X, float *Y, int i, float SA)
+        {
+            Y[i] = SA+X[i]+Y[i];
+        }
+
+    float *X, *Y, SA;
+
+int main(int argc, char *argv){
+
+
+
+}        
