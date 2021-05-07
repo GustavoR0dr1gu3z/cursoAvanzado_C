86fb139db9ba9d0ba986a3aed7cbcba45bea5e4d
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-07 12:17:47 -0500

    17.- For Colapsado

--- Programacion_Paralela/PARALELA/C++/17_FOR_COLAPSADO/fun_omp.h
+++ Programacion_Paralela/PARALELA/C++/17_FOR_COLAPSADO/fun_omp.h
@@ -1,3 +1,6 @@
 float* lee_vec(char*, int);
 float* crea_vec(int);
+float** lee_mat(char*, int, int);
+float** crea_mat(int, int);
+int muestra_mat(float**, int, int);
 int muestra_vec(float*, int);
