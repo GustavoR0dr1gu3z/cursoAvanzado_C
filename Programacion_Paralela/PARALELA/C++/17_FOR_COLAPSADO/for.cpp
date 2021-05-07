ca4587a6370cc85219df7726864f75194a76e8a2
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-07 12:05:32 -0500

    for colapsado

--- Programacion_Paralela/PARALELA/C++/17_FOR_COLAPSADO/for.cpp
+++ Programacion_Paralela/PARALELA/C++/17_FOR_COLAPSADO/for.cpp
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]){
     int i, j, m, n;
     m = 2;
     n = 5;
-    omp_set_nested(True);
+    omp_set_nested(true);
     A = crea_mat(m,2);
     #pragma omp for 
         for(i=0; i<m; i++){
@@ -29,3 +29,50 @@ int main(int argc, char* argv[]){
 
 return 0;
 }
+
+
+//CREA ESPACIO PARA LA MATRIZ, EN FORMA VECTORIZADA
+float** crea_mat(int m, int n){
+    int j;
+    float** M;
+    M = new float*[m];
+    for(j=0; j<m; j++){
+        M[j] = new float[n];
+    }
+return M;
+}
+
+// LLENA EL ESPACIO PARA LA MATRIZ
+float** lee_mat(char *nom_arch, int m, int n){
+    int i, j;
+    float **M;
+    M = crea_mat(m, n);
+    fstream fd1;
+    fd1.open(nom_arch, ios::in);
+    while(!fd1.eof()){
+        int i, j;
+        for (i=0; i<m; i++){
+            for (j=0; j<n; j++){
+                fd1>>M[i][j];
+            }
+        }
+    }
+    fd1.close();
+    cout<<"Matriz Leida"<<endl;
+return M;
+}
+
+
+
+// MUESTRA LA MATRIZ
+int muestra_mat(float** M, int m, int n){
+    int i, j;
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            cout<<M[i][j]<<", ";
+        }
+        cout<<endl;
+    }
+return 0;
+}
+
