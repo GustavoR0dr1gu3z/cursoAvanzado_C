fbb1ccf5c2f8d8cdf772855efbe183d4e0066567
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-13 17:58:49 -0500

    8 .- Vector por Vector Section

--- Programacion_Paralela/PARALELA/C++/08_SECTION_VxV/func_omp.cpp
+++ Programacion_Paralela/PARALELA/C++/08_SECTION_VxV/func_omp.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 #include <stdio.h>
+#include <fstream>
+#include <stdlib.h>
 
 using namespace std;
 
 float* lee_vec(char*, int);
 float* crea_vec(int);
 int muestra_vec(float*, int);
+
+
+
+float* crea_vec(int m){
+    float *W;
+    W = new float [m];      
+    return W;
+}
+
+
+int muestra_vec(float *M, int m){
+    int j;
+    for(j=0; j<m; j++){
+        cout<<M[j]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+
+float* lee_vec(char *nom_arch, int m){
+    float *W;
+    W = new float[m];
+    cout<<"Leyendo el vector "<<endl;
+    fstream fd;
+    fd.open(nom_arch, ios::in);
+    
+    while(!fd.eof()){
+        int i;
+        for(i=0; i<m; i++){
+            fd>>W[i];
+        }            
+    }
+    fd.close();
+    cout<<"Vector Leido "<<endl;
+    return W;
+}
