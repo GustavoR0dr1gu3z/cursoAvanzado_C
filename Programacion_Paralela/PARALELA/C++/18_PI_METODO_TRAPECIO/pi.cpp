22c186bec3f6f36a02d632011a3dbe9d357899df
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-13 22:22:57 -0500

    18.- PI Con El Metodo del Trapecio

--- Programacion_Paralela/PARALELA/C++/18_PI_METODO_TRAPECIO/pi.cpp
+++ Programacion_Paralela/PARALELA/C++/18_PI_METODO_TRAPECIO/pi.cpp
@@ -18,35 +18,34 @@ using namespace std;
 
 float ppi(int n, int A, int B){
     float pi = 0.0;
-    float subIntervalo = 0.0;
-    subIntervalo = ((B-A)/n);
+    float subIntervalo = ((B-A)/n);
     float h = subIntervalo/2;
-
+    float ba = B-A;
     float fa = 4/(1+0); // 4/(1+0̣^2)
     float fb = 4/(1+1); // 4/(1+1^2)
 
     float xk = A;
     // 4/(1+x'2)
     #pragma omp parallel for reduction(+:xk)
-        for(int i=0; i<n; i+= subIntervalo){
-            xk = xk+i;
+        for(int i=0; i<n; i++){
+            xk += subIntervalo;
         }
     pi = h * ( (fa) + (2*xk) + (fb) );
     return pi;
 }
 
-int main(int argc, char*argv[]){
+int main(int argc, char *argv[]){
     // n DEL POLIGONO
     int n;
     // LADOS A Y B
-    float A = 0;
-    float B = 1;
+    int A = 0;
+    int B = 1;
 
     // SE ASIGNAN LOS VALORES PARA LA FORMULA
     n = atoi(argv[1]);
 
     // SE MANDA A LLAMAR LA FUNCION DE PI
-    float cpi =0;
+    float cpi = 0.0;
     cpi = ppi(n,A,B);
 
     // SE MUESTRA EL RESULTADO
