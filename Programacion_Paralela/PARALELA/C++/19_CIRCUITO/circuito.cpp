32f5da6b83e1465f6d86a2f51daadf9d22b9a0dc
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-17 10:33:55 -0500

    19.- Circuito

--- Programacion_Paralela/PARALELA/C++/19_CIRCUITO/circuito.cpp
+++ Programacion_Paralela/PARALELA/C++/19_CIRCUITO/circuito.cpp
@@ -1,3 +1,12 @@
+// EJERCICIO 19: PROGRAMA CIRCUITO EN C++
+
+// Hecho por: Gustavo Rodriguez Calzada
+// El dia: 17 de Mayo del 2021
+
+// SE EJECUTA COMO: g++ -o circuitoCompilado circuito.cpp -fopenmp 
+// SE COMPILA COMO: ./circuitoCompilado 1000
+
+
 #include <iostream>
 #include <stdio.h>
 #include <omp.h>
