c0ede87d74db93d261c021bd4056eff4f44579f2
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-18 20:23:39 -0500

    21.- FLOYD Metodo Main

--- Programacion_Paralela/PARALELA/C++/21_FLOYD/floyd.cpp
+++ Programacion_Paralela/PARALELA/C++/21_FLOYD/floyd.cpp
@@ -13,3 +13,29 @@ Se ejecuta como: ./gaussCompilado 30 31
 #include <stdlib.h>
 #include <fstream>
 #include "func_omp.h"
+
+
+int main(int argc, char* argv[]){
+    int i,k,hi;
+    char matA[10000] = "matB.txt";
+    ren = atoi(argv[1]);
+    col = atoi(argv[2]);
+
+// SECTIONS: LEER Y MOSTRAR MATRIZ
+    //#pragma omp parallel private(tid)
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+            AU = crea_mat(ren,col);
+            AU = lee_mat(matA,ren,col);
+        }
+    }
+    //omp_set_num_threads(10);
+    //muestra_mat(AU,ren,col);
+    //X = gauss_f(AU, ren, col);
+    cout<<endl<<endl;
+    cout<<"El resultado es: "<<endl;
+    muestra_vec(X,ren);
+    return 0;
+}
