6839afd2e69af59f2dc8eee32ee06ac411ab6db6
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-18 20:22:48 -0500

    21.- FLOYD Bibliotecas

--- Programacion_Paralela/PARALELA/C++/21_FLOYD/floyd.cpp
+++ Programacion_Paralela/PARALELA/C++/21_FLOYD/floyd.cpp
@@ -7,3 +7,9 @@ El dia: 17 de Abril del 2021
 Se compila como: g++ -o gaussCompilado gauss.cpp -fopenmp
 Se ejecuta como: ./gaussCompilado 30 31
 */
+
+#include <iostream>
+#include <omp.h>
+#include <stdlib.h>
+#include <fstream>
+#include "func_omp.h"
