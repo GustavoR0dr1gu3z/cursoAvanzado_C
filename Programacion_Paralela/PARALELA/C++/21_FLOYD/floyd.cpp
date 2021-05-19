856c1a749becc294ae643bfbb0b18585c026b082
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-19 15:00:26 -0500

    21.- FLOYD LISTO

--- Programacion_Paralela/PARALELA/C++/21_FLOYD/floyd.cpp
+++ Programacion_Paralela/PARALELA/C++/21_FLOYD/floyd.cpp
@@ -132,7 +132,7 @@ float ** floyd_f(float **M, int m){
     cout<<"Matriz RESUELTA"<<endl;
     muestra_mat(M,m,m);
     cout<<endl;
-
+    cout<<endl;
     for(int i=0; i<m; i++){
         for(int j=0; j<m;j++){
             cout<<"De: "<<i<<" a: "<<j<<" : ";
