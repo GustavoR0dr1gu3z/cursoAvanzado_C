8776d78ac87f1af2c150faf5291820cb9c591322
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-20 17:40:06 -0500

    19.- Circuito Listo

--- Programacion_Paralela/PARALELA/C++/19_CIRCUITO/circuito.cpp
+++ Programacion_Paralela/PARALELA/C++/19_CIRCUITO/circuito.cpp
@@ -13,47 +13,43 @@ Se ejecuta como: ./circuitoCompilado
 
 using namespace std;
 
-// Regresa 1 si el bit i de n es 1, de lo contrario regresa 0
+//1 si el bit i de n es 1
+//0 si el bit i de n es 0
 #define EXTRACT_BIT(n, i) ((n&(1<<i))?1:0)
 
 void test_circuito(int, int);
 
 int main(int argc, char *argv[]){
 	int i;
-	int id;
-	int p;
+	int idHilos;
+	int numHilos;
 	omp_set_num_threads(10);
-	p = omp_get_num_threads();
-	id = omp_get_thread_num();
+	numHilos = omp_get_num_threads();
+	idHilos = omp_get_thread_num();
 	#pragma omp parallel for simd
-	for(i = id; i < 65536; i += p){
-		test_circuito(id, i);
+	for(i = idHilos; i < 65536; i += numHilos){
+		test_circuito(idHilos, i);
 	}
-	cout<<"El proceso " <<id<<" ha terminado"<<endl;
+	cout<<"ID del Hilo: " <<idHilos<<" ya terminó"<<endl;
 	fflush(stdout);
 	return 0;
 
 }
 
-void test_circuito(int id, int z){
-	int v[16];
+void test_circuito(int idHilo, int z){
+	int c[16];
 	int i;
 
-	for(i = 0; i < 16; i++) v[i] = EXTRACT_BIT(z,i);
-
-	if((v[0] || v[1]) && (!v[1] || !v[3])
-	    &&(v[2] || v[3]) && (!v[3] || !v[4])
-	    &&(v[4] || !v[5]) && (v[5] || !v[6])
-	    &&(v[5] || v[6]) && (v[6] || !v[15])
-	    &&(v[7] || !v[8]) && (!v[7] || !v[13])
-	    &&(v[8] || v[9]) && (v[8] || !v[9])
-	    &&(!v[9] || !v[10]) && (v[9] || v[11])
-	    &&(v[10] || v[11]) && (v[12] || v[13])
-	    &&(v[13] || !v[14]) && (v[14] || v[15])){
-		
-		cout<<id<<") "<<v[0]<<v[1]<<v[2]<<v[3]<<v[4]<<v[5]<<v[6]<<v[7]<<v[8]<<v[9]<<v[10]<<v[11]<<v[12]<<v[13]<<v[14]<<v[15]<<v[16]<<endl;
-		fflush(stdout);
+	for(i = 0; i < 16; i++){
+		c[i] = EXTRACT_BIT(z,i);
 	}
+
+	if((c[0] || c[1]) && (!c[1] || !c[3]) && (c[2] || c[3]) && (!c[3] || !c[4]) && (c[4] || !c[5]) && (c[5] || !c[6])
+		&&(c[5] || c[6]) && (c[6] || !c[15]) && (c[7] || !c[8]) && (!c[7] || !c[13]) && (c[8] || c[9]) && (c[8] || !c[9])
+		&&(!c[9] || !c[10]) && (c[9] || c[11]) && (c[10] || c[11]) && (c[12] || c[13]) && (c[13] || !c[14]) && (c[14] || c[15])){
+			cout<<idHilo<<") "<<c[0]<<c[1]<<c[2]<<c[3]<<c[4]<<c[5]<<c[6]<<c[7]<<c[8]<<c[9]<<c[10]<<c[11]<<c[12]<<c[13]<<c[14]<<c[15]<<c[16]<<endl;
+			fflush(stdout);
+		}
 
 
 }
