189cf192d29120309de7ed30926b0345f67832e4
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-31 19:15:02 -0500

    Terminando el programa

--- Programacion_Paralela/PARALELA/C++/23_Floyd/floyd.cpp
+++ Programacion_Paralela/PARALELA/C++/23_Floyd/floyd.cpp
@@ -33,7 +33,7 @@ int main(int argc, char*argv[]){
     for (k=0; k<tam; k++){
         for(i=0; i<tam; i++){
             for(j=0; j<tam; j++){
-                M[i][j] = min(A[i][j], (A[i][k],A[k][j]) );
+                M[i][j] = min(A[i][j], (A[i][k]+A[k][j]) );
                 A[i][j] = M[i][j];
             }
         }
